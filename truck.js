869b0d47ecd7c258e4610df52cd495eb8f606a9a
Mohamad Mohebifar <netfars@gmail.com>
2014-10-02 19:22:32 +0330

    Update Contents

--- truck.js
+++ truck.js
@@ -1,3 +1,13 @@
 /**
  * Created by mohamad on 10/2/14.
  */
+
+var commander = require('commander');
+var includeAll = require('include-all');
+
+function loadCommands(path) {
+    return includeAll({
+        dirname: require('path').resolve(__dirname, path),
+        filter: /(.+)\.js/
+    }) || {};
+}
